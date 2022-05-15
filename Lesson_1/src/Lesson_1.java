1171dd93e12727db228cc21b77ee44c25d56cc0a
Hucker9 <georgi.simonyan.100@gmail.com>
2022-05-15 08:37:25 -0700

    Lesson_1

---
+++ Lesson_1/src/Lesson_1.java
@@ -0,0 +1,14 @@
+public class Lesson_1 {
+    public static void main(String[] args) {
+        // popoxakanneri typer
+        //goyutyun uni mi qani tesakner typeri
+        short x = 1;
+        byte y = 2;
+        int z = 3;// javan bolor amboxj tver@ haskanum e int
+        long v = 4L;// dra hamarel longic araj dnum enq L tar@ vorovhetev inti u longi diapazonner tarber en
+        float b = 5f;//javan bolor iracional tver@ hamarume double nuyn patcharov  dra hamar krkin grum enq f floatic araj
+        double n = 8.6;
+        boolean a = true; // ays hramani mej ka erku arjeq true kam false
+        char o = 'a'; //chari mej karox enq pahel tar
+    }
+}
