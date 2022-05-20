24df368a9b73550ec0414617ad1f6bfd4d726885
Hucker9 <georgi.simonyan.100@gmail.com>
2022-05-20 12:27:18 -0700

    task 1

--- Homeworkes/src/Homeworks.java
+++ Homeworkes/src/Homeworks.java
@@ -1,10 +1,10 @@
 public class Homeworks {
     public static void main(String[] args) {
-        int[] arr = new int[5];
+        int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = 2;
+            arr[i] = i+1;
             for (int j = 0; j < 1; j++) {
-                System.out.print(arr[i] + " ");
+                System.out.print(arr[i]+" ");
             }
         }
     }
