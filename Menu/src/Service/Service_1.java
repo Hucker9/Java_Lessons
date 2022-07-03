6a30b86f917372dcaa8cfee264e2a2039c2dce70
Hucker9 <georgi.simonyan.100@gmail.com>
2022-07-03 13:18:34 -0700

    Menu

---
+++ Menu/src/Service/Service_1.java
@@ -0,0 +1,107 @@
+package Service;
+
+import Model.Student;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Service_1 extends Service {
+    public static Student studentCreate() {
+
+        Scanner m = new Scanner(System.in);
+        System.out.println("Enter the student name");
+        String name = m.next();
+        System.out.println("Enter the student lastname");
+        String lastName = m.next();
+        System.out.println("Enter the student year");
+        int year = m.nextInt();
+        System.out.println("Enter the student gender 'f' or 'm' ");
+        char gender = m.next().charAt(0);
+        System.out.println("Enter the student mark");
+        double mark = m.nextDouble();
+        Student student = new Student(name, lastName, gender, year, mark);
+        return student;
+    }
+
+    static Scanner y = new Scanner(System.in);
+
+    public static void write1() throws Exception {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students5 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students5[i] = studentCreate();
+        }
+        for (Student student : students5) {
+            Files.write(Paths.get("C:\\Users\\georg\\OneDrive\\Desktop\\" + student.getName()), student.toString1().getBytes());
+        }
+    }
+
+    public static void write() throws Exception {
+        System.out.println("how many students you want to create");
+        int c = y.nextInt();
+        int countOfStudents = c;
+        Student[] students6 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students6[i] = studentCreate();
+        }
+        Service.write(students6);
+    }
+
+    public static void theFemaleStudents() {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students[i] = studentCreate();
+        }
+        theFemaleStudents(students);
+    }
+
+    public static void theBestFemaleStudent() {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students1 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students1[i] = studentCreate();
+        }
+        theBestFemaleStudent(students1);
+    }
+
+    public static void sortByMarks() {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students2 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students2[i] = studentCreate();
+        }
+        sortByMarks(students2);
+    }
+
+    public static void candidateForRedDiploma() {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students3 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students3[i] = studentCreate();
+        }
+        candidateForRedDiploma(students3);
+    }
+
+    public static void expelledStudents() {
+        System.out.println("how many students you want to create");
+        int z = y.nextInt();
+        int countOfStudents = z;
+        Student[] students4 = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            students4[i] = studentCreate();
+        }
+        expelledStudents(students4);
+    }
+}
